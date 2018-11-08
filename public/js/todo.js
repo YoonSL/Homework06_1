bec1a0ac391c832d8d4290c689c2f08f9f917477
Yoonsub <yoonsub90@gmail.com>
2018-11-08 00:46:27 -0500

    somethings fixed and changed

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,90 +1,125 @@
 $(function () {
 
     const render = function () {
-        $('.content').empty();
-        runTodoQuery();
-    }
+        $('.content').html('');
+        $.ajax({url:'/api/todoList' ,method:'GET'})
+        .then(function(data){
+             for(let i = 0; i < data.length;i++){
 
-    const renderTodos = function (outputElement, dataList) {
-        for (let i = 0; i < dataList.length; i++) {
-            const output = $(outputElement);
-            const outputType = outputElement.slice(1);
-            const todoText = $(dataList[i]);
-            const todo = $('<div>').addClass('todoLists');
-            todo.append(
-                $('<button>')
+                $('.content').append(
+                    $('<button>')
                     .addClass(`far fa-square clear checkTodo`)
-                    .attr('data-index', `${outputType} - ${i}`),
+                    .attr('data-index', "heelp"),
                 $('<p>')
-                    .text(todoText)
-                    .addClass('textTodo')
-                    .attr('data-index', `${outputType} - ${i}`),
+                    .text(data[i])
+                    .addClass('textTodo'),
                 $('<button>')
-                    .addClass('fas fa-times clear deleteTodo')
-                    .attr('data-index', `${outputType} - ${i}`),
-            );
-            console.log(dataList[i]);
-            output.append(todo);
-        }
-    };
+                    .addClass('fas fa-times clear deleteTodo'),
+                )
 
-    const addingTodo = function () {
-        $.ajax({url: '/api/index',method:'POST'})
-        .then(function(todoList){
-            $('.content').empty();
-            renderTodos('.content',todoList);
-            console.log(todoList);
-        })
+                console.log(i);
+                console.log(data);
+             }
+            })
     }
-    const eraseTodo = function () {
-        const entry = $(this).attr('data-index');
 
-        const index = entry.split('_')[1];
-
-        $.ajax({ url: `/api/index/${index}`, method: 'DELETE' })
+    addingTodo = function(event){
+        event.preventDefault();
+        todoList = $('#add').val().trim();
+        const newdata= [todoList];
+        $.ajax({url:'api/todoList',method:'POST', newdata})
             .then(function (data) {
-                if (data.success) {
+                if (data) {
+                    $('#add').val('');
+                    $('#add').focus();
                     render();
                 } else {
-                    alert('There was a problem with your submission. Please check your entry and try again.');
+                    alert('Something is wrong');
                 }
-            }
-            )
-    }
-    const renderUpdateTodo = function(){
-        const entry = $(this).attr('data-index');
-        const i = parseInt(entry.split('_')[1]);
-
-        console.log(entry);
-        console.log(i);
-    }
-    const updateTodo = function(newTodo){
-        const changeTodo = [$('#add').val().trim()];
-        const index = newTodo.split('-')[1];
-
-        $.ajax(
-            {
-                url: `/api/index/${index}`,
-                method: 'PUT',
-                data:changeTodo
             })
-            .then(function(data){
-                if(data.success){
-                    render();
-                }
-            });
+        render();
     }
-    const runTodoQuery = function () {
-        $.ajax({ url: '/api/index', method: 'GET' })
-            .then(function (todoList) {
-                $('.content').empty();
-                renderTodos('.content', todoList);
-            })
-    }
-
 
     $('#buttonGo').on('click', addingTodo);
-    $(document).on('click', '.checkTodo', renderUpdateTodo);
-    $(document).on('click', '.deleteTodo', eraseTodo);
+    // $(document).on('click', '.checkTodo', renderUpdateTodo);
+    // $(document).on('click', '.deleteTodo', eraseTodo);
     render();
 });
+
+
+ // const renderTodos = function (outputElement, dataList) {
+    //     for (let i = 0; i < dataList.length; i++) {
+    //         const output = $(outputElement);
+    //         const outputType = outputElement.slice(1);
+    //         const todoText = $(dataList[i]);
+    //         const todo = $('<div>').addClass('todoLists');
+    //         todo.append(
+                // $('<button>')
+                //     .addClass(`far fa-square clear checkTodo`)
+                //     .attr('data-index', `${outputType} - ${i}`),
+                // $('<p>')
+                //     .text(todoText)
+                //     .addClass('textTodo')
+                //     .attr('data-index', `${outputType} - ${i}`),
+                // $('<button>')
+                //     .addClass('fas fa-times clear deleteTodo')
+                //     .attr('data-index', `${outputType} - ${i}`),
+    //         );
+    //         console.log(dataList[i]);
+    //         output.append(todo);
+    //     }
+    // };
+
+    // const addingTodo = function () {
+    //     $.ajax({url: '/api/todoList',method:'POST'})
+    //     .then(function(todoList){
+    //         $('.content').empty();
+    //         renderTodos('.content',todoList);
+    //         console.log(todoList);
+    //     })
+    // }
+    // const eraseTodo = function () {
+    //     const entry = $(this).attr('data-index');
+
+    //     const index = entry.split('_')[1];
+
+    //     $.ajax({ url: `/api/todoList/${index}`, method: 'DELETE' })
+    //         .then(function (data) {
+    //             if (data.success) {
+    //                 render();
+    //             } else {
+    //                 alert('There was a problem with your submission. Please check your entry and try again.');
+    //             }
+    //         }
+    //         )
+    // }
+    // const renderUpdateTodo = function(){
+    //     const entry = $(this).attr('data-index');
+    //     const i = parseInt(entry.split('_')[1]);
+
+    //     console.log(entry);
+    //     console.log(i);
+    // }
+    // const updateTodo = function(newTodo){
+    //     const changeTodo = [$('#add').val().trim()];
+    //     const index = newTodo.split('-')[1];
+
+    //     $.ajax(
+    //         {
+    //             url: `/api/todoList/${index}`,
+    //             method: 'PUT',
+    //             data:changeTodo
+    //         })
+    //         .then(function(data){
+    //             if(data.success){
+    //                 render();
+    //             }
+    //         });
+    // }
+    // const runTodoQuery = function () {
+    //     $.ajax({ url: '/api/todoList', method: 'GET' })
+    //         .then(function (todoList) {
+    //             $('.content').empty();
+    //             renderTodos('.content', todoList);
+    //         })
+    // }
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,16 +2,19 @@ const todoList = require('../data/todo-list.js');
 const sampleArray = require('../data/sample-array.json');
 
 module.exports = function (app) {
-    app.get('/api/index', function (req, res) {
+    app.get('/api/todoList', function (req, res) {
         res.json(todoList);
     })
 
-    app.post('/api/index',function(req, res){
+    app.post('/api/todoList',function(req, res){
         todoList.push(req.body);
         res.json(todoList);
     })
     
-    app.delete('/api/index/:index', function (req, res) {
+    app.put(`/api/todoList/:index`,function(req,res){
+        // todoList.splice(req.)
+    })
+    app.delete('/api/todoList/:index', function (req, res) {
         todoList.splice(req.params.index, 1);
 
         res.json({ success: true });
